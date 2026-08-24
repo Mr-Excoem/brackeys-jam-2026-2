e5ea0b5978658b342dcaf06bf9dc3f1f782d93eb
Rithunyaa <rithunyaa@gmail.com>
2026-08-23 20:09:46 -0400

    Basic Dialogue set up
    still have to do next button

--- brackeys-2026/main.gd
+++ brackeys-2026/main.gd
@@ -8,6 +8,17 @@ var correct_red = false
 var correct_blue = true
 var correct_green = false
 var bomb_defused = false
+var time_up = false
+
+# Dialogue
+var dialogue_index = 0
+
+var dialogue = [
+	{"speaker": "PERSON A", "text": "The blue light is on."},
+	{"speaker": "PERSON B", "text": "No, it isn't."},
+	{"speaker": "PERSON A", "text": "The red light is off."},
+	{"speaker": "PERSON B", "text": "The green light is on."}
+]
 
 func _on_red_light_pressed():
 	red_on = !red_on
@@ -25,14 +36,16 @@ func _on_green_light_pressed():
 	check_solution()
 
 func _process(delta):
-	if bomb_defused:
+	if bomb_defused or time_up:
 		return
 	
 	if time_left > 0:
 		time_left -= delta
 	
-	if time_left < 0:
+	if time_left <= 0:
 		time_left = 0
+		time_up = true
+		print("TIME'S UP!")
 	
 	var seconds = int(time_left)
 	$TimerLabel.text = "TIME: %02d" % seconds
@@ -41,3 +54,15 @@ func check_solution():
 	if red_on == correct_red and blue_on == correct_blue and green_on == correct_green:
 		bomb_defused = true
 		print("BOMB DEFUSED!")
+		
+func _ready():
+	$RedLight.text = "RED: OFF"
+	$BlueLight.text = "BLUE: OFF"
+	$GreenLight.text = "GREEN: OFF"
+	show_dialogue()
+	
+func show_dialogue():
+	$SpeakerLabel.text = dialogue[dialogue_index]["speaker"]
+	$DialogueLabel.text = dialogue[dialogue_index]["text"]
+		
+#proper combination:  red off, blue on, green off
