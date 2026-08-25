fd94b19bebdf27f1e19a6b9da20b5467c2956566
Mr-Excoem <chuikeeper@outlook.com>
2026-08-25 12:35:47 +0800

    Deleted some unused nodes and variables

--- brackeys-2026/main.gd
+++ brackeys-2026/main.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-var red_on = false
-var blue_on = false
-var green_on = false
-var time_left = 60.0
+#var time_left = 60.0
 var correct_red = false
 var correct_blue = true
 var correct_green = false
@@ -63,38 +60,19 @@ func _on_message_finished():
 @onready var bomb = $Bomb
 #in this case, red_on is bomb.red_button.on
 
-#
-func _on_red_light_pressed():
-	pass
-	#red_on = !red_on
-	#$RedLight.text = "RED: " + ("ON" if red_on else "OFF")
-	#check_solution()
-#
-func _on_blue_light_pressed():
-	pass
-	#blue_on = !blue_on
-	#$BlueLight.text = "BLUE: " + ("ON" if blue_on else "OFF")
-	#check_solution()
-#
-func _on_green_light_pressed():
-	pass
-	#green_on = !green_on
-	#$GreenLight.text = "GREEN: " + ("ON" if green_on else "OFF")
-	#check_solution()
-
 func _input(_event):
 	check_solution()
 
-func _process(delta):
+func _process(_delta):
 	
 	if bomb_defused or time_up:
 		return
 	
-	if time_left > 0:
-		time_left -= delta
+	#if time_left > 0:
+		#time_left -= delta
 	
-	if time_left <= 0:
-		time_left = 0
+	#if time_left <= 0:
+		#time_left = 0
 		#time_up = true
 		
 	
@@ -102,7 +80,7 @@ func _process(delta):
 		time_up = false
 		print("TIME'S UP!")
 	
-	var seconds = int(time_left)
+	#var seconds = int(time_left)
 	
 	#move the canva according to mouse movation
 	#and after game finished (bomb solved or exploded) this effect disappear
