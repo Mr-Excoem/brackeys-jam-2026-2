25a2d63bcaa4e0dbe4d962ced30eb4f0e5a839cb
Mr-Excoem <chuikeeper@outlook.com>
2026-08-30 13:15:26 +0800

    Added dialogue after bomb being defused

--- brackeys-2026/scripts/phone.gd
+++ brackeys-2026/scripts/phone.gd
@@ -6,14 +6,18 @@ const chat_message_scene = preload("uid://b8embku2jpi8")
 
 func add_dialogue(speaker: String, text: String):
 	if BombTimer.on_titlescreen:
-		return Signal()
+		return
 	
-	var message = chat_message_scene.instantiate()
+	var message:RichTextLabel = chat_message_scene.instantiate()
 	$ChatScroll/ChatMessages.add_child(message)
+	message.meta_clicked.connect(restart)
 	message.setup_message(speaker, text)
 	
 	await message.finished_typing
-	
+
+func _process(_delta):
+	@warning_ignore("narrowing_conversion")
 	chat_scroll.scroll_vertical = chat_scroll.get_v_scroll_bar().max_value
-	
-	return message.finished_typing
+
+func restart(_meta):
+	get_tree().reload_current_scene()
